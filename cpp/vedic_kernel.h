65a021f25c531d4cb1681d1f0a6ad88c8c161223
Joydeep Das | Divine Earthly <divineearthly@gmail.com>
2026-03-05 12:19:41 +0530

    Add header file vedic_kernel.h with function declarations for Vedic formulas

---
+++ cpp/vedic_kernel.h
@@ -0,0 +1,23 @@
+// vedic_kernel.h
+
+#ifndef VEDIC_KERNEL_H
+#define VEDIC_KERNEL_H
+
+// Function declarations for Vedic formulas
+
+// Function to calculate the square of a number
+double square(double x);
+
+// Function to calculate the cube of a number
+double cube(double x);
+
+// Function to calculate the sum of two numbers
+double sum(double a, double b);
+
+// Function to calculate the product of two numbers
+double product(double a, double b);
+
+// Function to calculate the average of an array of numbers
+double average(double* numbers, int size);
+
+#endif // VEDIC_KERNEL_H
